d98fc0036ef6ce50ead0225e16012cbbd66a974d
landa03 <60487799+landa03@users.noreply.github.com>
2020-09-21 18:04:42 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P3ValidationAlgorithm
+{
+    class Program
+    {
+        static int numero;
+        static void Main(string[] args)
+        {
+           System.Console.WriteLine("escrive la lonjitud de tu array");
+           numero = Convert.ToInt32(Console.ReadLine());
+           
+           for (int i = 1; i <= numero; i++)
+           {
+               System.Console.Write($"1,");
+           }
+           System.Console.Write(numero);
+        }
+    }
+}
